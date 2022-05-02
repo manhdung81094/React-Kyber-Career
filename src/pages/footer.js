3b17563cd6c17130c56c8a2e32a7d6f1a2dcb6e9
manhdung <manhdung81094@gmail.com>
2022-05-02 22:07:01 +0700

    add footer

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import UrlImage from '../image_upload/kyber-logo-text-white.svg'
 import '../css/footer.css'
+import LogoTwitter from '../image_upload/twitter.svg'
+import LogoDiscord from '../image_upload/discord.svg'
+import LogoTelegram from '../image_upload/telegram.svg'
+import LogoM from '../image_upload/medium-footer-icon.svg'
+import LogoYoutube from '../image_upload/youtube.svg'
+import LogoReddit from '../image_upload/reddit.svg'
+import LogoFacebook from '../image_upload/facebook.svg'
+import LogoGithub from '../image_upload/github.svg'
+import LogoBlockfolio from '../image_upload/ftx.svg'
 function Footer() {
     return (
         <div>
@@ -54,7 +63,54 @@ function Footer() {
                     <div className='col-md-6 d-flex align-items-center justify-content-center'>
                         <span>@ 2021 Kyber Network. All rights reserved.</span>
                     </div>
-                    <div className='col-md-6 d-flex align-items-center'>
+                    <div className='col-md-6 d-flex align-items-center justify-content-center'>
+                        <ul class="list-group list-group-horizontal">
+                            <li class="list-group-item border-0 bg-logo-footer">
+                                <a href='#!'>
+                                    <img src={LogoTwitter} className='img-fluid' alt='...' />
+                                </a>
+                            </li>
+                            <li class="list-group-item border-0 bg-logo-footer">
+                                <a href='#!'>
+                                    <img src={LogoDiscord} className='img-fluid' alt='...' />
+                                </a>
+                            </li>
+                            <li class="list-group-item border-0 bg-logo-footer">
+                                <a href='#!'>
+                                    <img src={LogoTelegram} className='img-fluid' alt='...' />
+                                </a>
+                            </li>
+                            <li class="list-group-item border-0 bg-logo-footer">
+                                <a href='#!'>
+                                    <img src={LogoM} className='img-fluid' alt='...' />
+                                </a>
+                            </li>
+                            <li class="list-group-item border-0 bg-logo-footer">
+                                <a href='#!'>
+                                    <img src={LogoYoutube} className='img-fluid' alt='...' />
+                                </a>
+                            </li>
+                            <li class="list-group-item border-0 bg-logo-footer">
+                                <a href='#!'>
+                                    <img src={LogoReddit} className='img-fluid' alt='...' />
+                                </a>
+                            </li>
+                            <li class="list-group-item border-0 bg-logo-footer">
+                                <a href='#!'>
+                                    <img src={LogoFacebook} className='img-fluid' alt='...' />
+                                </a>
+                            </li>
+                            <li class="list-group-item border-0 bg-logo-footer">
+                                <a href='#!'>
+                                    <img src={LogoGithub} className='img-fluid' alt='...' />
+                                </a>
+                            </li>
+                            <li class="list-group-item border-0 bg-logo-footer">
+                                <a href='#!'>
+                                    <img src={LogoBlockfolio} className='img-fluid' alt='...' />
+                                </a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </footer>
